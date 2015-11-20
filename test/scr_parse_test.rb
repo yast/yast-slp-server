7b2c69fd5de37a316cafbe730c9e2f12812d0ec1
Imobach González Sosa <igonzalezsosa@suse.com>
2015-11-20 17:03:51 +0000

    Replaces regexp tests by SCR ones

--- test/scr_parse_test.rb
+++ test/scr_parse_test.rb
@@ -3,35 +3,37 @@
 require_relative "test_helper"
 
 RSpec.describe "slp.conf parsing" do
-  COMMENTED_LINES = File.join(FIXTURES_PATH, "commented-slp.conf")
-  UNCOMMENTED_LINES = File.join(FIXTURES_PATH, "uncommented-slp.conf")
-
-  COMMENTED_REGEXP = Regexp.new '^[#;][ \t]*([^ \t\=]+)[ \t\=]*[ ]*(.+)[ \t]*$'
-  UNCOMMENTED_REGEXP = Regexp.new '[ \t]*([^ \t\=]+)[ \t\=]+[ ]*(.+)[ \t]*$'
-
-  File.readlines(COMMENTED_LINES).each do |line|
-    it "extracts param name and value from '#{line.chomp}'" do
-      match = COMMENTED_REGEXP.match(line)
-      expect(match).to_not be_nil, "Parsing failed"
-      expect(match[1]).to eq("foo")
-      expect(match[2]).to eq("bar")
+  around do |example|
+    change_scr_root(fixtures) do
+      example.run
     end
   end
 
-  File.readlines(UNCOMMENTED_LINES).each do |line|
-    it "extracts param name and value from '#{line.chomp}'" do
-      match = UNCOMMENTED_REGEXP.match(line)
-      expect(match).to_not be_nil, "Parsing failed"
-      expect(match[1]).to eq("foo")
-      expect(match[2]).to eq("bar")
+  context "given uncommented lines" do
+    let(:fixtures) { File.join(FIXTURES_PATH, "uncommented") }
+    UNCOMMENTED_LINE = { "comment" => "", "kind" => "value", "name" => "foo",
+                         "type" => 1, "value" => "bar" }
+
+    it "extract parts from each line" do
+      lines = Yast::SCR.Read(path(".etc.slp.all"))
+      expect(lines.size).to eq(6)
+      lines["value"].each do |line|
+        expect(line).to eq(UNCOMMENTED_LINE)
+      end
     end
   end
 
-  context "given a 'broken' line (bsc#878892)" do
-    it "extracts param name" do
-      match = COMMENTED_REGEXP.match("; foo ")
-      expect(match[1]).to eq("foo")
-      expect(match[2]).to eq(" ")
+  context "given commented lines" do
+    let(:fixtures) { File.join(FIXTURES_PATH, "commented") }
+
+    it "extract parts from each line starting with ';'" do
+      lines = Yast::SCR.Read(path(".etc.slp.all"))
+      lines["value"].each_with_index do |line, idx|
+        expect(line["name"]).to eq("foo#{idx}")
+        expect(line["value"]).to eq("bar")
+        expect(line["type"]).to eq(0)
+        expect(line["kind"]).to eq("value")
+      end
     end
   end
 end
