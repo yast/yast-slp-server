121795bc350aab788b2c6cc3ad2119f5785c6d3e
Knut Alejandro Anderssen González <teclator@gmail.com>
2020-03-09 08:51:00 +0000

    Add rubocop.

--- src/lib/y2slp/clients/slp_server.rb
+++ src/lib/y2slp/clients/slp_server.rb
@@ -19,7 +19,7 @@
 # To contact SUSE LLC about this file by physical or electronic mail, you may
 # find current contact information at www.suse.com.
 
-require 'yast'
+require "yast"
 
 module Y2Slp
   module Clients
@@ -29,14 +29,14 @@ module Y2Slp
 
       # Constructor
       def initialize
-        Yast.import 'UI'
-        textdomain 'slp-server'
+        Yast.import "UI"
+        textdomain "slp-server"
 
-        Yast.import 'Progress'
-        Yast.import 'Report'
-        Yast.import 'Summary'
-        Yast.import 'CommandLine'
-        Yast.include self, 'slp-server/wizards.rb'
+        Yast.import "Progress"
+        Yast.import "Report"
+        Yast.import "Summary"
+        Yast.import "CommandLine"
+        Yast.include self, "slp-server/wizards.rb"
       end
 
       def main
@@ -45,50 +45,50 @@ module Y2Slp
         end
       end
 
-      private
+    private
 
       # It logs the start and finish of the given block call returning the
       # result of the call.
       def log_and_return(&block)
         # The main ()
-        log.info '----------------------------------------'
-        log.info 'SlpServer module started'
+        log.info "----------------------------------------"
+        log.info "SlpServer module started"
 
         ret = block.call
 
         # Finish
         log.info "SlpServer module finished with ret=#{ret.inspect})"
-        log.info '----------------------------------------'
+        log.info "----------------------------------------"
       end
 
       def cmdline_description
         {
-          'id' => 'slp-server',
+          "id"         => "slp-server",
           # Command line help text for the Xslp-server module
-          'help' => _(
-            'Configuration of an SLP server'
+          "help"       => _(
+            "Configuration of an SLP server"
           ),
-          'guihandler' => fun_ref(method(:SlpServerSequence), 'any ()'),
-          'initialize' => fun_ref(Yast::SlpServer.method(:Read), 'boolean ()'),
-          'finish' => fun_ref(Yast::SlpServer.method(:Write), 'boolean ()'),
-          'actions' =>
-            # FIXME: TODO: fill the functionality description here
-            {},
-          'options' =>
-            # FIXME: TODO: fill the option descriptions here
-            {},
-          'mappings' =>
-            # FIXME: TODO: fill the mappings of actions and options here
-            {}
+          "guihandler" => fun_ref(method(:SlpServerSequence), "any ()"),
+          "initialize" => fun_ref(Yast::SlpServer.method(:Read), "boolean ()"),
+          "finish"     => fun_ref(Yast::SlpServer.method(:Write), "boolean ()"),
+          "actions"    =>
+                          # FIXME: TODO: fill the functionality description here
+                          {},
+          "options"    =>
+                          # FIXME: TODO: fill the option descriptions here
+                          {},
+          "mappings"   =>
+                          # FIXME: TODO: fill the mappings of actions and options here
+                          {}
         }
       end
 
       def propose?
         return false if Yast::WFM.Args.empty?
         return false unless Yast::Ops.is_path?(Yast::WFM.Args.first)
-        return false unless Yast::WFM.Args.first == path('.propose')
+        return false unless Yast::WFM.Args.first == path(".propose")
 
-        log.info 'Using PROPOSE mode'
+        log.info "Using PROPOSE mode"
         true
       end
     end
